1aadda5c47411ff14ffa67c69a124f62986da571
ikki-0914 <k.ikki050914@gmail.com>
2026-04-28 23:24:30 +0900

    firsttest

---
+++ app/src/main/java/jp/ac/gifu_u/info/kondo/test1/MainActivity.java
@@ -0,0 +1,41 @@
+package jp.ac.gifu_u.info.kondo.test1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        showToast("アプリは終了しました");
+        finish();
+    }
+
+    public void showToast(String string){
+        Toast t = Toast.makeText(
+                this, string, Toast.LENGTH_SHORT);
+        t.show();
+    }
+}
